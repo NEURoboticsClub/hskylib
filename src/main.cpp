3fba4b7961732942329301de997316b40af066f9
Ctgoyette <ctgoyette21@gmail.com>
2024-11-20 19:08:42 -0500

    Modified makefile to build core repo submodule

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,8 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	std::array<float, 12> thing = {0};
+	UpdateTuningDisplay(thing);
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
