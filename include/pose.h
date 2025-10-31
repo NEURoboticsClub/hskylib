b0c5d0bad70787a9a671d43abd41a93002e8febf
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-10-30 20:35:43 -0400

    Fixed pose

--- include/pose.h
+++ include/pose.h
@@ -14,7 +14,7 @@ struct Pose {
 
 /**
  * Comparison operator for Pose to enable use as a map key.
- * Compares lexicographically by x, then y, then theta.
+ * Compares by x, then y, then theta.
  */
 inline bool operator<(const Pose &a, const Pose &b) {
 	return std::tie(a.x, a.y, a.theta) < std::tie(b.x, b.y, b.theta);
